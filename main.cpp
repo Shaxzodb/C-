abeb6e81a555938f9f0a0d337cd7b307c137e4db
SHAXZOD <shaxzodbmaster@gmail.com>
2022-04-04 11:06:12 +0500

    C++ updatemain.cpp

--- main.cpp
+++ main.cpp
@@ -7,10 +7,10 @@ void Cars(){
     Car car=*new Car("BMW",300,5);
     car.AllCar();
     MyCar *mycar=new MyCar("BMW","Shaxzod",340,4);
-    cout << mycar -> All() << endl;
-}
+    cout << mycar->All() << endl;
+};
 
 int main(){
     Cars();
     return 0;
-}
+};
